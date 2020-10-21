b902af4e96a2cbbe19e883fe46dbb97ae91854a8
James Conant <jconant1993@gmail.com>
2020-10-21 14:13:51 -0500

    Fixed 'contentHash' -> 'hash' in webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "[name].[contentHash].bundle.js",
+    filename: "[name].[hash].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   optimization: {
@@ -28,7 +28,7 @@ module.exports = merge(common, {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
+    new MiniCssExtractPlugin({ filename: "[name].[hash].css" }),
     new CleanWebpackPlugin(),
   ],
   module: {
